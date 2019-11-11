18f884975079dffda2d9768c4a2c7fa48b3af332
Kevin Huang <keh034@ucsd.edu>
2019-11-10 16:00:10 -0800

    update myListings and App.js for viewListings x4

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import HomePage from './src/screens/HomePage';
 import SellRent from './src/screens/SellRent';
 import Report from './src/screens/Report';
 import PriceTrends from './src/screens/PriceTrends';
-import viewListing from './src/screens/viewListing';
+import viewListingsBuyer from "./src/screens/viewListingsBuyer";
+import viewListingsRentee from "./src/screens/viewListingsRentee";
+import viewListingsSeller from "./src/screens/viewListingsSeller";
+import viewListingsRenter from "./src/screens/viewListingsRenter";
 import BuyRent from './src/screens/BuyRent';
 import myListings from "./src/screens/myListings";
 import Form from './src/screens/Form';
@@ -18,16 +21,18 @@ const navigator = createStackNavigator(
   {
    
 
-    Page: HomePage,
-    SR : SellRent,
-    Rep : Report,
-    Price : PriceTrends,
-    Listing : viewListing,
-    myListings: myListings,
-    BR : BuyRent,
-    form : Form,
-   
 
+      Page: HomePage,
+      SR : SellRent,
+      Rep : Report,
+      Price : PriceTrends,
+      ListingBuyer : viewListingsBuyer,
+      ListingRentee : viewListingsRentee,
+      ListingSeller : viewListingsSeller,
+      ListingRenter : viewListingsRenter,
+      myListings: myListings,
+      BR : BuyRent,
+      form : Form,
 
   },
   {
