dc86310e52def33e8a7ac75dd91fa1f8716a0886
felips-1 <56095791+felips-1@users.noreply.github.com>
2019-11-15 14:33:49 -0800

    Add files via upload

---
+++ src/screens/base.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const db = firebase.initializeApp ({
+    apiKey: "AIzaSyA6V2uc7dtaPdB2OqizjAj_AtfsXrfRfOk",
+    authDomain: "tritonclick-83081.firebaseapp.com",
+    databaseURL: "https://tritonclick-83081.firebaseio.com",
+    projectId: "tritonclick-83081",
+    storageBucket: "tritonclick-83081.appspot.com",
+    messagingSenderId: "78488626956",
+    appId: "1:78488626956:web:12382422753bda30ea0b29",
+    measurementId: "G-DDR0GEFXL8"
+  });
+
+  export default db;
