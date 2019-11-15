12bd2e2f6e7299468c72becf7589347752d041cf
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2019-11-15 15:40:09 -0800

    front end implmenetation of Buyer and Renter

--- src/screens/viewListingsBuyer.js
+++ src/screens/viewListingsBuyer.js
@@ -1,8 +1,89 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 
-const viewListingsBuyer = () => {
-    return <Text>This is the view listings for the Buyer Page</Text>
-}
 
-export default viewListingsBuyer;
+export default class viewListingsBuyer extends React.Component {
+    render() {
+
+      return (
+
+        <View style={styles.container}>
+  
+          <ScrollView>
+
+            {/* Inputs for Email, Barcode, Price, iClicker, Sell Option, Picture, */}
+            <View style={styles.inputContainer}>
+  
+              <Text>Seller Email goes here</Text>
+  
+              <Text>Price goes here</Text>
+
+              <Text>Type of iClicker goes here</Text>
+  
+              <Text>Condition of iClicker goes here</Text>
+  
+              {/* TODO Display Picture*/}
+  
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.saveButton}
+                >
+                  <Text style={styles.saveButtonText} onPress={() => alert("You have responded to Seller")} >Email</Text>
+                </TouchableOpacity>
+
+              </View>
+  
+            </View>
+          </ScrollView>
+  
+        </View>
+      );
+    };
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 45,
+      backgroundColor: '#F5FCFF',
+    },
+    header: {
+      fontSize: 25,
+      textAlign: 'center',
+      margin: 10,
+      fontWeight: 'bold'
+    },
+    inputContainer: {
+      paddingTop: 15
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    textInput: {
+      borderColor: '#CCCCCC',
+      borderTopWidth: 1,
+      borderBottomWidth: 1,
+      height: 50,
+      fontSize: 25,
+      paddingLeft: 20,
+      paddingRight: 20,
+      marginBottom: 20
+    },
+    saveButton: {
+      borderWidth: 1,
+      borderColor: '#007BFF',
+      backgroundColor: '#007BFF',
+      padding: 15,
+      margin: 5
+    },
+    saveButtonText: {
+      color: '#FFFFFF',
+      fontSize: 20,
+      textAlign: 'center'
+    },
+    text: {
+      fontSize: 12
+    }
+  
+  });
--- src/screens/viewListingsRent.js
+++ src/screens/viewListingsRent.js
@@ -1,8 +1,89 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 
-const viewListingsRent = () => {
-    return <Text>This is the view listings for the person who wants to rent an iClicker for cheaper</Text>
-}
 
-export default viewListingsRent;
+export default class viewListingsRent extends React.Component {
+    render() {
+  
+      return (
+
+        <View style={styles.container}>
+  
+          <ScrollView>
+
+            {/* Inputs for Email, Barcode, Price, iClicker, Sell Option, Picture, */}
+            <View style={styles.inputContainer}>
+  
+              <Text>Seller Email goes here</Text>
+  
+              <Text>Price goes here</Text>
+
+              <Text>Type of iClicker goes here</Text>
+  
+              <Text>Condition of iClicker goes here</Text>
+  
+              {/* TODO Display Picture*/}
+  
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.saveButton}
+                >
+                  <Text style={styles.saveButtonText} onPress={() => alert("You have responded to Loaner")} >Email</Text>
+                </TouchableOpacity>
+
+              </View>
+  
+            </View>
+          </ScrollView>
+  
+        </View>
+      );
+    };
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 45,
+      backgroundColor: '#F5FCFF',
+    },
+    header: {
+      fontSize: 25,
+      textAlign: 'center',
+      margin: 10,
+      fontWeight: 'bold'
+    },
+    inputContainer: {
+      paddingTop: 15
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    textInput: {
+      borderColor: '#CCCCCC',
+      borderTopWidth: 1,
+      borderBottomWidth: 1,
+      height: 50,
+      fontSize: 25,
+      paddingLeft: 20,
+      paddingRight: 20,
+      marginBottom: 20
+    },
+    saveButton: {
+      borderWidth: 1,
+      borderColor: '#007BFF',
+      backgroundColor: '#007BFF',
+      padding: 15,
+      margin: 5
+    },
+    saveButtonText: {
+      color: '#FFFFFF',
+      fontSize: 20,
+      textAlign: 'center'
+    },
+    text: {
+      fontSize: 12
+    }
+  
+  });
