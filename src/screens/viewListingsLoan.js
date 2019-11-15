826b55b98d24f59176962ec3255669613ed6f756
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2019-11-15 15:25:29 -0800

    updated const names for viewListings

--- src/screens/viewListingsLoan.js
+++ src/screens/viewListingsLoan.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const viewListingsRenter = () => {
+const viewListingsLoan = () => {
     return <Text>This is the view listings for the person renting out their iClicker</Text>
 }
 
-export default viewListingsRenter;
+export default viewListingsLoan;
--- src/screens/viewListingsRent.js
+++ src/screens/viewListingsRent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const viewListingsRentee = () => {
-    return <Text>This is the view listings for the person who wants to rent an iClciker for cheaper</Text>
+const viewListingsRent = () => {
+    return <Text>This is the view listings for the person who wants to rent an iClicker for cheaper</Text>
 }
 
-export default viewListingsRentee;
+export default viewListingsRent;
