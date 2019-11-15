23aed3238a2a730ca0a06eaa525b0cd1e9a0b88a
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2019-11-15 15:23:05 -0800

    corrected viewListing names

--- src/screens/viewListingsLoan.js
+++ src/screens/viewListingsLoan.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const viewListingsRenter = () => {
-    return <Text>This is the view listings for the Renter Page</Text>
+    return <Text>This is the view listings for the person renting out their iClicker</Text>
 }
 
 export default viewListingsRenter;
