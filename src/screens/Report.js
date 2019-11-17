ab94b31dd5f4e52e0bcdfe8fa064affca32b4740
Tahmid- <tahmidkhan2@gmail.com>
2019-11-16 17:50:07 -0800

    Delete Report.js

--- src/screens/Report.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const Report = () => {
-    return <Text>This is the Report Page</Text>
-}
-export default Report;
