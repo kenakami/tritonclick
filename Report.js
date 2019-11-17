803cc0ef52fb7b3a9b9c704def15221c79064a9d
Tahmid- <tahmidkhan2@gmail.com>
2019-11-16 17:48:01 -0800

    Add files via upload

---
+++ Report.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+
+export default class Settings extends React.Component {
+  render() {
+
+    let type = [{
+      value: 'on',
+    }, {
+      value: 'off',
+    }];
+
+
+
+
+    return (
+
+      <View style={styles.container}>
+
+        <ScrollView>
+          <View style={styles.inputContainer}>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder="Feedback"
+              maxLength={100}
+              keyboardType="default"
+              autoCorrect={true}
+            />
+            <View style={styles.inputContainer}>
+              <TouchableOpacity
+                style={styles.saveButton}
+              >
+                <Text style={styles.saveButtonText} onPress={() => alert("Thank you for your feedback")} >Submit</Text>
+              </TouchableOpacity>
+            </View>
+
+			<View style={styles.inputContainer}>
+			  <TouchableOpacity
+				style={styles.saveButton}
+			  >
+				<Text style={styles.saveButtonText} onPress={() => alert("Redirecting")} >View Terms and Conditions</Text>
+			  </TouchableOpacity>
+			</View>
+
+            <Dropdown
+              label='Notifications'
+              data={type}
+            />
+
+
+
+
+            <View style={styles.inputContainer}>
+              <TouchableOpacity
+                style={styles.saveButton}
+              >
+                <Text style={styles.saveButtonText} onPress={() => alert("Your preferences have been saved")} >Save</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.inputContainer}>
+              <TouchableOpacity
+                style={styles.saveButton}
+              >
+                <Text style={styles.saveButtonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+
+
+
+
+
+          </View>
+        </ScrollView>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold'
+  },
+  inputContainer: {
+    paddingTop: 15
+  },
+  textInput: {
+    borderColor: '#CCCCCC',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 20
+  },
+  saveButton: {
+    borderWidth: 1,
+    borderColor: '#007BFF',
+    backgroundColor: '#007BFF',
+    padding: 15,
+    margin: 5
+  },
+  saveButtonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 12
+  }
+
+});
