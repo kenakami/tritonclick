14e094b2a443f8368be9290422c90c09f46641e6
tahmidkhan2@gmail.com <tahmidkhan2@gmail.com>
2019-12-06 16:53:50 -0800

    changes

--- src/screens/viewListingsSeller.js
+++ src/screens/viewListingsSeller.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image} from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
@@ -41,18 +41,18 @@ export default class viewListingsSeller extends React.Component {
 
   deleteListing(clickerid) {
     const { currentUser } = firebase.auth();
+    firebase.storage().ref(`${this.state.barcode}`).remove();
     firebase.database().ref(`/users/${currentUser.uid}/Selling/${clickerid}`).remove();
   }
 
-  storeSale(clickerType, price, type, condition){
+  storeSale(clickerType, price, condition){
     // Stores in database for price trends
     if(clickerType == 'iClicker 2'){
-      let date = new Date().toLocaleString().split(" ");
       firebase.database().ref(`sales/`).push({
         price,
         timestamp: new Date().toLocaleString(),
-        month: date[1],
-        type,
+        month: new Date().getMonth()+1,
+        clickerType,
         condition,
       }).then((data) => {
         //success callback
@@ -68,20 +68,27 @@ export default class viewListingsSeller extends React.Component {
     // Get listing information through param
     const { navigation } = this.props;
     const clickerid = navigation.getParam('clickerid', 'default');
+
 	this.currentUser =  firebase.auth().currentUser;
+
+    const image = navigation.getParam('Image', 'default');
+
+
     function Separator() {
       return <View style={styles.separator} />;
     }
 
     const dd_type = [{value: 'iClicker 1',}, {value: 'iClicker 2',}];
     const dd_cond = [{value: 'Like New',}, {value: 'Used',}];
-    const trans = [{value: 'Selling',}, {value: 'Renting Out',}];
 
     return (
 
         <View style={styles.container}>
 
           <ScrollView>
+            <View style={styles.imageContainer}>
+              <Image source={{uri: image}} style={styles.image}/>
+            </View>
 
             {/* Inputs for Email, Barcode, Price, iClicker, Sell Option, Picture, */}
             <View style={styles.inputContainer}>
@@ -177,7 +184,7 @@ export default class viewListingsSeller extends React.Component {
                                           {text: 'Cancel', style: 'cancel'},
                                           {text: 'OK', onPress: () =>{
                                               this.deleteListing(clickerid),
-                                                  this.storeSale(this.state.type, this.state.price, this.state.type, this.state.condition),
+                                                  this.storeSale(this.state.type, this.state.price, this.state.condition),
                                                   this.props.navigation.navigate('Page') }
                                           }
                                         ]
@@ -217,6 +224,16 @@ export default class viewListingsSeller extends React.Component {
 }
 
 const styles = EStyleSheet.create({
+  image: {
+    flex: 1,
+    width: '20rem',
+    height: '16rem',
+    resizeMode: 'contain',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     paddingTop: 45,
